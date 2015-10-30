20a061cd578b5c26f9c0fd552a9d10f60adc54ee
Aakash1987 <aakashgupta18@gmail.com>
2015-10-29 23:32:09 -0700

    Pseudo code - Part 1

---
+++ pokerface.py
@@ -0,0 +1,23 @@
+#Pseudo Code for Poker Face v1
+
+#Defining a deck of cards from 1.1 to 4.13
+
+Deck_of_cards = [1,2,3,4]
+temp_array=[]
+y=1
+t=0
+for i in Deck_of_Cards
+	for y<14
+		t=i+0.1*y
+		Add t to temp_array
+		y=y+1
+		
+Append temp_array to deck_of_cards
+
+#Shuffle the deck of cards
+random.shuffle(deck_of_cards)
+
+#Define game structure
+rounds=0
+players=2
+

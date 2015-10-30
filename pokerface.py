82a677439782f04f1629a83d0d122653c8421c58
Aakash1987 <aakashgupta18@gmail.com>
2015-10-29 23:58:14 -0700

    Pseudo Code edit

--- pokerface.py
+++ pokerface.py
@@ -1,6 +1,6 @@
 #Pseudo Code for Poker Face v1
 
-#Defining a deck of cards from 1.1 to 4.13
+#Defining a deck of cards from 1.1 to 4.13 - Not needed for v1
 
 Deck_of_cards = [1,2,3,4]
 temp_array=[]
@@ -14,10 +14,30 @@ for i in Deck_of_Cards
 		
 Append temp_array to deck_of_cards
 
-#Shuffle the deck of cards
+#Shuffle the deck of cards - Not needed for v1
 random.shuffle(deck_of_cards)
 
-#Define game structure
-rounds=0
-players=2
 
+#Define game structure
+
+rounds = 1
+chance = random.random()
+
+for rounds<5
+	{
+	player=1
+	rounds=rounds+1
+	
+	for player<3
+		{
+		if chance<0.5, decision=0 else decision=1
+	
+		if decision=0, then {rounds=5, winner=player, player = 3}
+		else continue
+		player=player+1
+		}
+	}
+	
+
+Print: Winner is player number
+Print: winner

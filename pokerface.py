f07faeb16d363fe14ea2d71059129195db7c147f
Aakash1987 <aakashgupta18@gmail.com>
2015-11-09 01:54:16 -0800

    Closes issue #1

--- pokerface.py
+++ pokerface.py
@@ -1,14 +1,33 @@
 import random
 import sys
 random.seed(int(sys.argv[1]))
-rounds=1
+rounds=0
 decision = 0
+final_round=5
+	
+
 while rounds<5:
+
+	player=1
 	rounds=rounds+1
-	chance=random.random()
-	if chance<0.5:
-		decision = 1
-		final_round=rounds
+	
+	while player<3:
+		chance=random.random()
+		# print player
+		print chance
+		if chance<0.5:
+			decision = 1
+			final_round=rounds
+			loser = player
+			break
+		player = player + 1
+		if decision == 0:
+			loser = 0
+			
+	if decision ==1:
 		break
 
+
 print decision
+print final_round
+print loser

6e05536be79bbe47605b41a38687206f2938ed01
Aakash1987 <aakashgupta18@gmail.com>
2015-11-09 15:03:48 -0800

    Solves issue #2

--- pokerface.py
+++ pokerface.py
@@ -6,6 +6,33 @@ decision = 0
 final_round=5
 print "Chances of each player per round were:"
 
+#1-Spades, 2-Clubs, 3-Diamonds, 4- Hearts
+# 1.1- Ace through 1.13-King, 1 - 10
+deck_of_cards = [1,2,3,4]
+temp_array=[]
+t=0
+
+for i in deck_of_cards:
+	y=1
+	while y<14:
+		if y<10:
+			t=i+0.1*y
+			print t
+			temp_array.append(t)
+			y=y+1	
+		if y>10:
+			t=i+0.01*y
+			print t
+			temp_array.append(t)
+			y=y+1	
+		if y==10:
+			y=y+1
+		
+		
+#Append temp_array to deck_of_cards
+deck_of_cards.extend(temp_array)
+
+
 while rounds<5:
 
 	player=1
@@ -33,3 +60,5 @@ print "The final round was:"
 print final_round
 print "The loser was:"
 print loser
+print temp_array
+print deck_of_cards
